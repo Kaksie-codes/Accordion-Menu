931fe8aac1219214a5a54fca6449664c9ad765e3
Nsikak Akpan <113060075+Kaksie-codes@users.noreply.github.com>
2022-12-25 12:37:05 +0100

    initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//DOM Manipulators
+const accordion = document.getElementsByClassName('content-container');
+console.log(accordion)
+
+for( i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    })
+}
